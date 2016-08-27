359e77e778c9437475bf30ec4f63b7a569c3626d
smyzin <smyzin.work@gmail.com>
2016-08-27 17:01:46 +0300

    First commit
    Add files to repo

---
+++ assets/js/function.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	console.log('Done');
+	// $(document).on('mousemove touchmove', (e1) =>{
+	// 			$('.interactive').on('mouseleave touchend', (e2)  => {
+	// 				var topPos = e2.pageY - 34;
+	// 				var leftPos = e2.pageX - 78;
+	// 				$('#drag').css({
+	// 					'top': topPos + 'px',
+	// 					'left': leftPos + 'px',
+	// 					'display': 'block'
+	// 				});
+	// 			});
+
+	// 			$('#person').on('mouseenter touchstart', () => {
+	// 				$('#drag').css('display', 'none');
+	// 				$('#person img').on( "mousedown touchend", ( event )  => {
+	// 					$('#person').addClass('happy');
+	// 				});
+	// 			});
+
+	// 			if($('#person').hasClass('happy')){
+	// 				$('#person img').attr({'src': '/assets/img/happy.png', 'alt': 'Happy person'});
+	// 				$('.interactive').css('cursor', 'default');
+	// 				$('#drag').css('display', 'none');
+	// 			}
+	// 		});
+
+});
