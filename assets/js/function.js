7f36db785c240d9bdb59726427825d5f4518c29c
smyzin <smyzin.work@gmail.com>
2016-08-29 15:40:40 +0300

    Update
    Fix some issue & add slide-show

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,28 +1,25 @@
 $(document).ready(function(){
-	console.log('Done');
-	// $(document).on('mousemove touchmove', (e1) =>{
-	// 			$('.interactive').on('mouseleave touchend', (e2)  => {
-	// 				var topPos = e2.pageY - 34;
-	// 				var leftPos = e2.pageX - 78;
-	// 				$('#drag').css({
-	// 					'top': topPos + 'px',
-	// 					'left': leftPos + 'px',
-	// 					'display': 'block'
-	// 				});
-	// 			});
+	// Remember clicked btn start
+	$('.buttons_btn a').on('click', function(ev){
+		ev.preventDefault();
 
-	// 			$('#person').on('mouseenter touchstart', () => {
-	// 				$('#drag').css('display', 'none');
-	// 				$('#person img').on( "mousedown touchend", ( event )  => {
-	// 					$('#person').addClass('happy');
-	// 				});
-	// 			});
+		$('.buttons_btn a').removeClass('pressed');
+		$(this).addClass('pressed');
 
-	// 			if($('#person').hasClass('happy')){
-	// 				$('#person img').attr({'src': '/assets/img/happy.png', 'alt': 'Happy person'});
-	// 				$('.interactive').css('cursor', 'default');
-	// 				$('#drag').css('display', 'none');
-	// 			}
-	// 		});
+		var btnVal = $(this).text();
+
+		switch(btnVal){
+			case 'день':
+				$('#buttons_btn12').addClass('pressed')
+				break;
+			case 'неделя':
+				$('#buttons_btn22').addClass('pressed')
+				break;
+			case 'месяц':
+				$('#buttons_btn32').addClass('pressed')
+				break;
+		};
+	});
+	// Remember clicked btn end
 
 });
